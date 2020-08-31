cb7f839377d307ee3a3b1659488e2b8f9a4681e1
CT <uhjio1233@gmail.com>
2020-08-31 13:11:25 +0800

    add lib

--- CSV/CSVreadANDwrie.java
+++ CSV/CSVreadANDwrie.java
@@ -10,9 +10,9 @@ import java.io.InputStreamReader;
 public class CSVreadANDwrie {
 	public static void main(String[] args) throws IOException{
 		try {
-			InputStreamReader fil=new InputStreamReader(new FileInputStream("D:/apiCLASS/simpleCSV.csv"));
+			InputStreamReader fil=new InputStreamReader(new FileInputStream("G:/json_test/simpleCSV.csv"));
 		    BufferedReader Bfil=new BufferedReader(fil);
-		    File crefil=new File("D:/outputCSV.csv");
+		    File crefil=new File("G:/json_test/outputCSV.csv");
 		    BufferedWriter BWfil=new BufferedWriter(new FileWriter (crefil));
 		    String line=null;
 		    line=Bfil.readLine();
